42c22c0f527e2275d96d0dbceded81751a5b595b
August Lilleaas <august@augustl.com>
2014-06-29 02:15:17 +0200

    Make it so.

---
+++ README.md
@@ -0,0 +1,9 @@
+# augusts-fancy-blog-post-parser
+
+Just a small Clojure library I use to parse my home made HTML based templating system for my blogs augustl.com and dbs-are-fn.com.
+
+## License
+
+Copyright © 2014 August Lilleaas
+
+Distributed under BSD 3-clause license.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject augusts-fancy-blog-post-parser "0.1.0"
+  :description "A fancy (silly?)  parser for my blog posts"
+  :license {:name "BSD 3-clause"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [enlive "1.1.5"]
+                 [clygments "0.1.1"]])
